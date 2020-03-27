2f3ca871cfe24f51800396f398b5cea5523ef416
Daria <daria.shtanko@gmail.com>
2020-03-27 10:39:42 +0100

    script finished

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-const panels = document.querySelectorAll(".panel");
+window.onload = () => {
+  const panels = document.querySelectorAll(".panel");
 
-//window.onload = my_code();
+  //window.onload = my_code();
+
+  function toggleOpen() {
+    console.log("Hello");
+    this.classList.toggle("open");
+  }
+
+  function toggleActive(e) {
+    console.log(e.propertyName);
+    if (e.propertyName.includes("flex")) {
+      this.classList.toggle("open-active");
+    }
+  }
+
+  panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+  panels.forEach(panel =>
+    panel.addEventListener("transitionend", toggleActive)
+  );
+};
